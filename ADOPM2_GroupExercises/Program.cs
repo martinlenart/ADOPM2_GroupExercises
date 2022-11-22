e96de4d06c99e2598373e9982e698979eb187455
Martin Lenart <martin_lenart@icloud.com>
2022-11-22 12:29:12 +0100

    Add project files.

---
+++ ADOPM2_GroupExercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADOPM2_GroupExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
