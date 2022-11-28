8654d4371a35421620d32d977a8e0c3dd3b3493b
Martin Lenart <martin_lenart@icloud.com>
2022-11-28 20:46:49 +0100

    update

---
+++ Yahtzee/IDice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    public enum DiceFace { One = 1, Two, Three, Four, Five, Six }
+    internal interface IDice
+    {
+        public DiceFace Face { get; init; }
+    }
+}
---
+++ Yahtzee/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Yahtzee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test creating a couple of Dice
+            IDice dice1 = null;
+            IDice dice2 = null;
+            Console.WriteLine(dice1);
+            Console.WriteLine(dice2);
+
+            //Test creating an empty cup
+            ICupOfDices cup1 = null;
+            Console.WriteLine(cup1);
+
+            //Test creating a cup with 5 dice
+            ICupOfDices cup2 = null;
+            Console.WriteLine(cup2);
+
+            //Shake the cup
+            //cup2.Shake();
+            Console.WriteLine(cup2);
+
+            //Sort the cup
+            //cup2.Sort();
+            Console.WriteLine(cup2);
+
+            //check if you have Yahtzee
+            //Console.WriteLine(cup2.IsYahtzee());
+
+        }
+    }
+}
