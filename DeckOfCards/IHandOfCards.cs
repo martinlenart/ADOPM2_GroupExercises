903feb0eb96b56bb1ee3207d0c79cc0f6df21c4c
Martin Lenart <martin_lenart@icloud.com>
2022-11-28 20:28:08 +0100

    update

---
+++ DeckOfCards/IHandOfCards.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPartBAnswers_B2
+{
+    interface IHandOfCards
+    {
+        /// <summary>
+        /// Adds a card into the hand
+        /// </summary>
+        /// <param name="card"></param>
+        void Add(IPlayingCard card);
+
+        /// <summary>
+        /// Epmties the hand, i.e. all cards removed
+        /// </summary>
+        void Clear();
+    }
+}
