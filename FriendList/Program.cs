26e1e036568ba2f33aeabaedfeb85a70b809517e
Martin Lenart <martin_lenart@icloud.com>
2022-12-01 15:40:02 +0100

    update

--- FriendList/Program.cs
+++ FriendList/Program.cs
@@ -9,43 +9,7 @@ namespace FriendList // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            #region Step 1
-            Friend f2 = new Friend("Anne", "Sterling", "a.sterling@icloud.com",
-                new AddressType { Street = "Backstreet 3", Zip = 98765, City = "Sigtuna", Country = "Sweden" });
-            Console.WriteLine(f2);
 
-            Friend f3 = Friend.Factory.CreateRandom();
-            Console.WriteLine(f3);
-
-            List<Friend> myFriends = new List<Friend>();
-            for (int i = 0; i < 10_000; i++)
-            {
-                myFriends.Add(Friend.Factory.CreateRandom());
-            }
-
-            Console.WriteLine("\nFirst 20");
-            for (int i = 0; i < 20; i++)
-            {
-                Console.WriteLine(myFriends[i]);
-            }
-            Console.WriteLine("\nLast 20");
-            for (int i = 0; i < 20; i++)
-            {
-                Console.WriteLine(myFriends[myFriends.Count-1-i]);
-            }
-            #endregion
-
-            #region Step 2 and 3
-            Console.WriteLine("\nFriendList - friends1");
-            var friends1 = FriendList.Factory.CreateRandom(10);
-            Console.WriteLine(friends1);
-
-            Console.WriteLine("\nFriendList - friends2");
-            var friends2 = FriendList.Factory.CreateRandom(5);
-            Console.WriteLine(friends2);
-
-            friends1.myFriends = null;
-            #endregion
         }
     }
 }
