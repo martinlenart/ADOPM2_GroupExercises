8a6d963cc47abd59a37be24dff124772cf807db5
Martin Lenart <martin_lenart@icloud.com>
2022-12-07 15:14:07 +0100

    updated

---
+++ DeepCopy/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepCopy
+{
+    public class Person:IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public override string ToString() => $"Hi, I'm {FirstName} {LastName}. I was is born on {Birthday:d} " +
+            $"and you can reach me on {Email}";
+
+        //Class factory
+        public static class Factory
+        {
+            public static Person CreateRandom()
+            {
+                IGetRandom rnd = new GetRandom();
+
+            }
+        }
+
+        //Copy constructor
+     }
+}
---
+++ DeepCopy/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DeepCopy;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPerson aFriend = Person.Factory.CreateRandom();
+        Console.WriteLine(aFriend);
+
+        //Shallow copy of a friend
+        IPerson anotherFriend = aFriend;
+        Console.WriteLine(anotherFriend);
+
+        aFriend.FirstName = "Donald";
+        aFriend.LastName = "Duck";
+        Console.WriteLine(anotherFriend);
+
+        //Write code for Deep copy of a friend
+ 
+        //List of persons
+        Person[] list = new Person[20];
+        for (int i = 0; i < list.Length; i++)
+        {
+            list[i] = Person.Factory.CreateRandom();
+        }
+
+        //Shallow list copy
+        Person[] people = new Person[20];
+        Array.Copy(list, people, list.Length);
+
+        list[0].FirstName = "Donald";
+        list[0].LastName = "Duck";
+        Console.WriteLine(list[0]);
+        Console.WriteLine(people[0]);
+
+        //write code for Deep list copy
+      }
+}
+
