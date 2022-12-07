a082bbc59906ff9d5107a193073d573fa0ae7d5b
Martin Lenart <martin_lenart@icloud.com>
2022-12-07 15:54:04 +0100

    update

---
+++ IEquatable_IComparable/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace IEquatable_IComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPerson> persons = new List<IPerson>();
+            for (int i = 0; i < 100; i++)
+            {
+                persons.Add(Person.Factory.CreateRandom());
+            }
+
+            var aPerson = new Person((Person) persons[0]);
+            Console.WriteLine(persons.Contains(aPerson));
+
+            persons.Sort();
+            Console.WriteLine(persons);
+        }
+    }
+}
